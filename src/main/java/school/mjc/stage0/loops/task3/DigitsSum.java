8d0996432abaae572bcdf32d9f0bde77d04fb5e7
Constantine <77935386+velteren@users.noreply.github.com>
2023-12-18 16:12:26 +0300

    fix: errors

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,13 +2,16 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-        int result = 0;
-        int sum = 0;
-        t = Math.abs(t);
-        for (int i = t; i >= 0; i /= 10) {
-            result = i % 10;
-            sum += result;
+        if(t<0){
+            t*=-1;
         }
-        System.out.println(result);
+        int sum=0;
+        int res=0;
+        for(int i=t;i>0;i/=10){
+            res = i%10;
+            sum+=res;
+        }
+        System.out.println(sum);
+
     }
 }
