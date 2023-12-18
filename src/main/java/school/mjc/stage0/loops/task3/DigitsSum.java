d38ae1441f3a9d012aad8fe366b7942e1fb58320
Constantine <77935386+velteren@users.noreply.github.com>
2023-12-18 15:53:16 +0300

    fix: errors

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -3,8 +3,11 @@ package school.mjc.stage0.loops.task3;
 public class DigitsSum {
     public void printDigitsSum(int t){
         int result = 0;
-        for (int i = 0; i < (t+"").length(); i++) {
-            result += (int) ((t+"").charAt(i));
+        int sum = 0;
+        t = Math.abs(t);
+        for (int i = t; i > 0; i /= 10) {
+            result = i % 10;
+            sum += result;
         }
         System.out.println(result);
     }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -4,7 +4,7 @@ public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
         int result = 0;
         int tmp = 9;
-        for (int i = 1; i <= lengthOfLastNumber; i++) {
+        for (int i = 1; i < lengthOfLastNumber; i++) {
             tmp = tmp * 10 + 9;
             result += tmp;
         }
